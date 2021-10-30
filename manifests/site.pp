425c5de1e09425064e657eecd8213e3347f3dd91
pecbo <87227694+pecbo@users.noreply.github.com>
2021-10-30 15:07:58 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,7 +5,7 @@ node 'puppet.local' {
   include role::master_server
   file { '/root/README':
     ensure => file,
-    content => "Welcome to ${fqdn} running on ${os.family}",
+    content => "Welcome to ${fqdn}",
   }
 }
 
